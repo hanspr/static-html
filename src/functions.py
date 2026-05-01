1182fa702a5e169e001f7e25cd86dcc0a2058804
Hans Peyrot <hans@eflow.com.mx>
2026-05-01 09:46:50 -0600

    reorganize code and namespaces

--- src/functions.py
+++ src/functions.py
@@ -1,12 +1,24 @@
 import re
 import textnode as tn
 import htmlnode as hn
+#from pprint import pprint
 
 def extract_markdown_images(text):
     return re.findall(r"!\[(.+?)\]\((.+?)\)", text)
 
 def extract_markdown_links(text):
     return re.findall(r"[^!]\[(.+?)\]\((.+?)\)", text)
+
+def text_node_to_html_node(text_node):
+    prop = None
+    if text_node.text_type == tn.TextType.TEXT:
+        return hn.LeafNode(None, text_node.text, None, prop)
+    if text_node.text_type == tn.TextType.LINK:
+        prop = {"href":text_node.url}
+    if text_node.text_type == tn.TextType.IMAGE:
+        prop = {"src":text_node.url, "alt":text_node.text}
+        text_node.text = ""
+    return hn.LeafNode(text_node.text_type.value, text_node.text, None, prop)
 
 def split_nodes_image(old_nodes):
     new_nodes = []
@@ -48,37 +60,39 @@ def split_nodes_link(old_nodes):
                 new_nodes.append(tn.TextNode(part, tn.TextType.TEXT))
     return new_nodes
 
-def text_node_to_html_node(text_node):
-    prop = None
-    if text_node.text_type == tn.TextType.TEXT:
-        return hn.LeafNode(None, text_node.text, None, prop)
-    if text_node.text_type == tn.TextType.LINK:
-        prop = {"href":text_node.url}
-    if text_node.text_type == tn.TextType.IMAGE:
-        prop = {"src":text_node.url, "alt":text_node.text}
-        text_node.text = ""
-    return hn.LeafNode(text_node.text_type.value, text_node.text, None, prop)
-
-def split_nodes_delimeter(o_node, delimeter = None, text_type = None):
+def split_nodes_delimeter(old_nodes, delimeter = None):
     new_nodes = []
-    if (delimeter == None):
-        new_nodes.append(o_node)
-    elif text_type != tn.TextType.TEXT:
-        new_nodes.append(o_node)
-    else:
-        parts = o_node.text.split(delimeter)
-        if len(parts)%2 == 0:
-            raise Exception("invalid markdown syntax")
-        for i in range(0, len(parts)):
-            if i%2 == 0:
-                new_nodes.append(tn.TextNode(parts[i], text_type))
-            else:
-                type = text_type
-                if delimeter == "_":
-                    type = tn.TextType.ITALIC
-                elif delimeter == "**":
-                    type = tn.TextType.BOLD
-                elif delimeter == "`":
-                    type = tn.TextType.CODE
-                new_nodes.append(tn.TextNode(parts[i], type))
+    for node in old_nodes:
+        if (delimeter == None):
+            new_nodes.append(node)
+        elif node.text_type != tn.TextType.TEXT:
+            new_nodes.append(node)
+        else:
+            parts = node.text.split(delimeter)
+            if len(parts)%2 == 0:
+                raise Exception("invalid markdown syntax")
+            for i in range(0, len(parts)):
+                if i%2 == 0:
+                    new_nodes.append(tn.TextNode(parts[i], node.text_type))
+                else:
+                    type = node.text_type
+                    if delimeter == "_":
+                        type = tn.TextType.ITALIC
+                    elif delimeter == "**":
+                        type = tn.TextType.BOLD
+                    elif delimeter == "`":
+                        type = tn.TextType.CODE
+                    new_nodes.append(tn.TextNode(parts[i], type))
     return new_nodes
+
+def text_to_textnodes(text):
+    text_node = tn.TextNode(text, tn.TextType.TEXT)
+    nodes = split_nodes_image([text_node])
+    nodes = split_nodes_link(nodes)
+    for delimeter in ("_", "**", "`"):
+        nodes = split_nodes_delimeter(nodes, delimeter)
+    return nodes
+
+#text = "This is **text** with an _italic_ word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
+#pprint(text_to_textnodes(text), indent = 4)
+#print(text_to_textnodes(text))
--- src/test_split_nodes.py
+++ src/test_split_nodes.py
@@ -7,30 +7,30 @@ class TestTextNode(unittest.TestCase):
     def test_split_node_error(self):
         node = tn.TextNode("This is a _text node", tn.TextType.TEXT)
         with self.assertRaises(Exception):
-            fn.split_nodes_delimeter(node, "_", node.text_type)
+            fn.split_nodes_delimeter([node], "_")
     
     def test_split_not_TEXT(self):
         node = tn.TextNode("This is a _italic text_ node", tn.TextType.CODE)
-        new_nodes = fn.split_nodes_delimeter(node, "_", node.text_type)
+        new_nodes = fn.split_nodes_delimeter([node], "_")
         self.assertEqual(node, new_nodes[0])
         
     def test_split_italic(self):
         node = tn.TextNode("This is a _italic text_ node", tn.TextType.TEXT)
-        new_nodes = fn.split_nodes_delimeter(node, "_", node.text_type)
+        new_nodes = fn.split_nodes_delimeter([node], "_")
         self.assertEqual(f"{new_nodes[0]!r}", 'TextNode(This is a ,TEXT,None)')
         self.assertEqual(f"{new_nodes[1]!r}", 'TextNode(italic text,ITALIC,None)')
         self.assertEqual(f"{new_nodes[2]!r}", 'TextNode( node,TEXT,None)')
             
     def test_split_bold(self):
         node = tn.TextNode("This is a **bold text** node", tn.TextType.TEXT)
-        new_nodes = fn.split_nodes_delimeter(node, "**", node.text_type)
+        new_nodes = fn.split_nodes_delimeter([node], "**")
         self.assertEqual(f"{new_nodes[0]!r}", 'TextNode(This is a ,TEXT,None)')
         self.assertEqual(f"{new_nodes[1]!r}", 'TextNode(bold text,BOLD,None)')
         self.assertEqual(f"{new_nodes[2]!r}", 'TextNode( node,TEXT,None)')
             
     def test_split_multi_bold(self):
         node = tn.TextNode("This **is** a multi **bold text** node", tn.TextType.TEXT)
-        new_nodes = fn.split_nodes_delimeter(node, "**", node.text_type)
+        new_nodes = fn.split_nodes_delimeter([node], "**")
         self.assertEqual(f"{new_nodes[0]!r}", 'TextNode(This ,TEXT,None)')
         self.assertEqual(f"{new_nodes[1]!r}", 'TextNode(is,BOLD,None)')
         self.assertEqual(f"{new_nodes[2]!r}", 'TextNode( a multi ,TEXT,None)')
